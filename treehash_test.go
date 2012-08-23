11c09ba5d0cfbf37b697ec624386e439a404afbe
Timothée Peignier <timothee.peignier@tryphon.org>
2012-08-23 22:04:40 +0200

    add a naive test

--- treehash_test.go
+++ treehash_test.go
@@ -1,8 +1,19 @@
 package treehash
 
 import (
+	"io"
+	"fmt"
 	"testing"
 )
+
+func TestTreeHash(t *testing.T) {
+	th := New()
+	io.WriteString(th, "Even if I could be Shakespeare, I think I should still choose to be Faraday. - A. Huxley")
+	s := fmt.Sprintf("%x", th.Sum(nil))
+	if s != "1fb2eb3688093c4a3f80cd87a5547e2ce940a4f923243a79a2a1e242220693ac" {
+		t.Fatal("wrong checksum")
+	}
+}
 
 func BenchmarkTreeHash(b *testing.B) {
 	b.StopTimer()
